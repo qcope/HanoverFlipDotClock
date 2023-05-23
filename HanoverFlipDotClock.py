3dc6470e99b7b8cef87507a6b883dd42bb6c85d9
Quentin Cope <qcope@hotmail.com>
2023-05-23 21:26:53 +0100

    Fixing lost port change

--- HanoverFlipDotClock.py
+++ HanoverFlipDotClock.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 from HanoverDisplay import HanoverDisplay
 
-display=HanoverDisplay("COM4")
+HANOVER_PORT "/dev/ttyUSB0"       # COM4 or /dev/ttyUSB0 are good choices
+
+display=HanoverDisplay(HANOVER_PORT)
 
 while True:
     now_time=datetime.now()
